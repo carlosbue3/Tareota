ed9bd46128b5b3cda72e0c63f9baba114edf1f47
carlosbue3 <141794660+carlosbue3@users.noreply.github.com>
2025-04-19 17:22:56 -0500

    d
    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 
 import './App.css'
 import Menuu from './Componentes/Menuu'
+import Headerr from "./Componentes/Headerr"
 
 
 function App() {
   
 
   return (
-    <>
+    <><div>
       <Menuu></Menuu>
+
+<div>
+  <Headerr></Headerr>
+</div>
+
+
+      </div>
     </>
   )
 }
---
+++ src/Componentes/Headerr.jsx
@@ -0,0 +1,18 @@
+import Loguito from "../assets/Loguito.png"
+
+function Headerr() {
+  return (
+    <div class="Container">
+    <div class="diva"> 
+        <h1>We Care About</h1>
+        <h1>Your Healt</h1>
+
+    </div>
+    <div class="divb"> </div>
+    <div class="divc"> </div>
+    <div class="divd"> </div>
+    </div>
+  );
+}
+
+export default Headerr;
