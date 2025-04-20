f51defececf56943961ce33ad0d7dc791e98c75b
carlosbue3 <141794660+carlosbue3@users.noreply.github.com>
2025-04-19 22:36:24 -0500

    a
    f

--- src/Componentes/Headerr.jsx
+++ src/Componentes/Headerr.jsx
@@ -1,17 +1,33 @@
-import Loguito from "../assets/Loguito.png"
-
+import dogs from "../assets/dogs.jpg"
+import gg from "../assets/gg.mp3"
 function Headerr() {
   return (
     <div class="Container">
     <div class="diva"> 
         <h1>We Care About</h1>
         <h1>Your Healt</h1>
+        <h6>Mucho texto afefffqfqfqfqfqftgrgrgr</h6>
 
     </div>
-    <div class="divb"> </div>
-    <div class="divc"> </div>
-    <div class="divd"> </div>
+    <div class="divb">
+      
+    <form>
+    <input type="text" /><button>BUSCAR</button></form>
+      
+       </div>
+    <div class="divc"><button>Escuchar</button></div>
+    <div className="divd"><audio controls src={gg}></audio></div>
+    <div class="dive"><img className="hh" src={dogs} alt="dadad" /> </div>
     </div>
+
+
+    
+
+
+
+
+
+
   );
 }
 
